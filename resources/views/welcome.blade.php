610d07552fe869c62e1a859ea5659a44b07db961
charles.muller <charlesmuller@rede.ulbra.br>
2022-11-23 21:22:52 -0300

    ajuste

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,8 +36,8 @@
                 </div>
             @endif
 
-            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                
+            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">cd 
+                qualquer coisa
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
